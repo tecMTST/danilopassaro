335acdda7ac155b6c21c77b18f41bb466f172cab
Daniel Silva <devmaquina.ds@gmail.com>
2024-07-20 14:43:32 -0300

    Implementa menu mobile

---
+++ js/script.js
@@ -0,0 +1,8 @@
+document.querySelector('.hamburger-menu').addEventListener('click', function() {
+    const nav = document.querySelector('nav');
+    if (nav.style.display === 'flex') {
+        nav.style.display = 'none';
+    } else {
+        nav.style.display = 'flex';
+    }
+});
