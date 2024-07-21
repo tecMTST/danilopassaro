b09edfc36c73c1c0070455a5d17a9210b16c30aa
Daniel Silva <devmaquina.ds@gmail.com>
2024-07-21 16:08:28 -0300

    Ajuste mobile página O que defendemos

--- js/script.js
+++ js/script.js
@@ -1,37 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const hamburgerMenu = document.querySelector('.hamburger-menu');
-    const nav = document.querySelector('nav');
-
-    // Função para alternar a visibilidade do menu
-    function toggleMenu() {
-        if (window.getComputedStyle(nav).display === 'none' || nav.style.display === 'none') {
-            nav.style.display = 'flex';
-        } else {
-            nav.style.display = 'none';
-        }
-    }
-
-    // Evento de clique no botão do menu hambúrguer
-    hamburgerMenu.addEventListener('click', toggleMenu);
-
-    // Função para ajustar a exibição do menu com base no tamanho da tela
-    function adjustMenu() {
-        if (window.innerWidth > 992) {
-            nav.style.display = 'flex';
-        } else {
-            nav.style.display = 'none';
-        }
-    }
-
-    // Ajustar a exibição do menu ao redimensionar a janela
-    window.addEventListener('resize', adjustMenu);
-
-    // Verificar a exibição do menu no carregamento da página
-    adjustMenu();
-});
-
-
-document.addEventListener('DOMContentLoaded', function() {
     // Código do menu hambúrguer
     const hamburgerMenu = document.querySelector('.hamburger-menu');
     const nav = document.querySelector('nav');
@@ -78,4 +45,3 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
-
