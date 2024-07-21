2e14e25c047396d3243877cd5f3bb18a184cc5ae
Daniel Silva <devmaquina.ds@gmail.com>
2024-07-21 15:01:01 -0300

    finaliza a pagina o que defendemos desktop

--- js/script.js
+++ js/script.js
@@ -29,3 +29,53 @@ document.addEventListener('DOMContentLoaded', function() {
     // Verificar a exibição do menu no carregamento da página
     adjustMenu();
 });
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Código do menu hambúrguer
+    const hamburgerMenu = document.querySelector('.hamburger-menu');
+    const nav = document.querySelector('nav');
+
+    function toggleMenu() {
+        if (window.getComputedStyle(nav).display === 'none' || nav.style.display === 'none') {
+            nav.style.display = 'flex';
+        } else {
+            nav.style.display = 'none';
+        }
+    }
+
+    hamburgerMenu.addEventListener('click', toggleMenu);
+
+    function adjustMenu() {
+        if (window.innerWidth > 992) {
+            nav.style.display = 'flex';
+        } else {
+            nav.style.display = 'none';
+        }
+    }
+
+    window.addEventListener('resize', adjustMenu);
+    adjustMenu();
+
+    // Código do accordion
+    const accordionHeaders = document.querySelectorAll('.header-accordion');
+
+    accordionHeaders.forEach(header => {
+        header.addEventListener('click', function() {
+            const content = header.nextElementSibling;
+            const arrow = header.querySelector('.arrow-icon');
+
+            // Alternar a visibilidade do conteúdo
+            if (header.classList.contains('open')) {
+                content.style.maxHeight = null;
+                header.classList.remove('open');
+                arrow.src = '../img/fi-rr-caret-down.svg';
+            } else {
+                content.style.maxHeight = content.scrollHeight + "px";
+                header.classList.add('open');
+                arrow.src = '../img/fi-rr-caret-up.svg';
+            }
+        });
+    });
+});
+
